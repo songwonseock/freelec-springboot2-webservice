42283dfee922ec012fb04d2aceb11fb244656ea0
songwonseock <tablier@gmail.com>
2020-01-15 00:22:32 +0900

    [수정] script 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,13 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=freelec-springboot2-webservice
 
-
 echo "> Build 파일 복사"
 
-
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
-
 
 echo "> 현재구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
-
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -38,6 +34,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,classpath:/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 
